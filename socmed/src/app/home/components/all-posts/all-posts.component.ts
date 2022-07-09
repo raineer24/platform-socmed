1a0a080d95bffb8ff90fa9f493915fbfd05dd07e
wtf <mura man kag wtf>
2022-07-09 20:45:47 +0800

    infinite scroll integration

--- socmed/src/app/home/components/all-posts/all-posts.component.ts
+++ socmed/src/app/home/components/all-posts/all-posts.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { Post } from '../../models/post.interface';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-all-posts',
@@ -6,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-posts.component.scss'],
 })
 export class AllPostsComponent implements OnInit {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
-  constructor() { }
+  queryParams: string;
+  allLoadedPosts: Post[] = [];
+  numberOfPosts = 5;
+  skipPosts = 0;
 
-  ngOnInit() {}
+  constructor(private postsService: PostService) {}
 
+  ngOnInit() {
+    this.getPosts(false, '');
+  }
+
+  getPosts(isInitialLoad: boolean, event) {
+    if (this.skipPosts === 20) {
+      event.target.disabled = true;
+    }
+    this.queryParams = `?take=${this.numberOfPosts}&skip=${this.skipPosts}`;
+
+    this.postsService.getSelectedPosts(this.queryParams).subscribe(
+      (posts: Post[]) => {
+        // eslint-disable-next-line @typescript-eslint/prefer-for-of
+        for (let post = 0; post < posts.length; post++) {
+          this.allLoadedPosts.push(posts[post]);
+        }
+        if (isInitialLoad) {
+          // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+          event.target.complete();
+        }
+        this.skipPosts = this.skipPosts + 5;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  loadData(event) {
+    this.getPosts(true, event);
+  }
 }
