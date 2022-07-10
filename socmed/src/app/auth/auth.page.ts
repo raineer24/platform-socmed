5fc4a56d7a96a84bb7b8fb7f0dbd9a58c4c5e4b1
wtf <mura man kag wtf>
2022-07-10 23:39:53 +0800

    form ngModel

--- socmed/src/app/auth/auth.page.ts
+++ socmed/src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+  @ViewChild('form') form: NgForm;
 
-  constructor() { }
+  submissionType: 'login' | 'join' = 'login';
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSubmit() {
+    const { email, password } = this.form.value;
+
+    if (!email || !password) {
+      return;
+    }
+
+    if (this.submissionType === 'login') {
+      console.log(1, 'handle login', email, password);
+    } else if (this.submissionType === 'join') {
+      const { firstName, lastName } = this.form.value;
+      if (!firstName || !lastName) {
+        return;
+      }
+      console.log(1, 'handle join', email, password, firstName, lastName);
+    }
   }
 
+  toggleText() {
+    if (this.submissionType === 'login') {
+      this.submissionType = 'join';
+    } else if (this.submissionType === 'join') {
+      this.submissionType = 'login';
+    }
+  }
 }
