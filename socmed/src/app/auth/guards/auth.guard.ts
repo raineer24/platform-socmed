2fd98f2b554c60e1db1d3474f22b5c743777b783
wtf <mura man kag wtf>
2022-07-12 22:21:13 +0800

    canLoad auth guard

--- socmed/src/app/auth/guards/auth.guard.ts
+++ socmed/src/app/auth/guards/auth.guard.ts
@@ -1,15 +1,44 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
+} from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { switchMap, take, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+export class AuthGuard implements CanLoad {
+  constructor(private authService: AuthService, private router: Router) {}
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.authService.isUserLoggedIn.pipe(
+      take(1),
+      switchMap((isUserLoggedIn: boolean) => {
+        if (isUserLoggedIn) {
+          return of(isUserLoggedIn);
+        }
+        // return this.authService.isToke
+      }),
+      tap((isUserLoggedIn: boolean) => {
+        if (!isUserLoggedIn) {
+          this.router.navigateByUrl('/auth');
+        }
+      })
+    );
   }
-  
 }
