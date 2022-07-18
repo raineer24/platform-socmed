4b0764c535e5586deb74fd156ed0f5a245430472
wtf <mura man kag wtf>
2022-07-18 20:24:14 +0800

    image path column

--- api/src/auth/services/user.service.ts
+++ api/src/auth/services/user.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { from, map, Observable } from 'rxjs';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { User } from '../models/user.class';
 import { UserEntity } from '../models/user.entity';
 
@@ -11,7 +10,6 @@ export class UserService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
-    private jwtService: JwtService,
   ) {}
   findUserById(id: number): Observable<User> {
     return from(
@@ -25,6 +23,22 @@ export class UserService {
       map((user: User) => {
         delete user.password;
         return user;
+      }),
+    );
+  }
+
+  updateUserImageById(id: number, imagePath: string): Observable<UpdateResult> {
+    const user: User = new UserEntity();
+    user.id = id;
+    user.imagePath = imagePath;
+    return from(this.userRepository.update(id, user));
+  }
+
+  findImageNameByUserId(id: number): Observable<string> {
+    return from(this.userRepository.findOne({ where: { id } })).pipe(
+      map((user: User) => {
+        delete user.password;
+        return user.imagePath;
       }),
     );
   }
