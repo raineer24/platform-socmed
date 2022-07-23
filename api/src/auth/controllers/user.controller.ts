780834e483366df0dac6e265544fc76fed94c375
wtf <mura man kag wtf>
2022-07-23 15:29:10 +0800

    image-name

--- api/src/auth/controllers/user.controller.ts
+++ api/src/auth/controllers/user.controller.ts
@@ -58,4 +58,15 @@ export class UserController {
       }),
     );
   }
+
+  @UseGuards(JwtGuard)
+  @Get('image-name')
+  findUserImageName(@Request() req, @Res() res): Observable<object> {
+    const userId = req.user.id;
+    return this.userService.findImageNameByUserId(userId).pipe(
+      switchMap((imageName: string) => {
+        return of({ imageName });
+      }),
+    );
+  }
 }
