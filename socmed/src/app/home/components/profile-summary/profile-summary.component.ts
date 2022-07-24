278e0dc9732ffeb6cdce3688117ea5ba16abeb31
wtf <mura man kag wtf>
2022-07-24 22:41:17 +0800

    buffer file type

--- socmed/src/app/home/components/profile-summary/profile-summary.component.ts
+++ socmed/src/app/home/components/profile-summary/profile-summary.component.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
-import { switchMap, take } from 'rxjs/operators';
+import { buffer, switchMap, take } from 'rxjs/operators';
 import { Role } from 'src/app/auth/models/user.model';
 import { FormControl, FormGroup } from '@angular/forms';
+import { fromBuffer } from 'file-type/core';
+import { from, of } from 'rxjs';
+import { FileTypeResult } from 'file-type';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+type validFileExtension = 'png' | 'jpg' | 'jpeg';
+// eslint-disable-next-line @typescript-eslint/naming-convention
+type validMimeType = 'image/png' | 'image/jpg' | 'image/jpeg';
+
 type BannerColors = {
   colorOne: string;
   colorTwo: string;
@@ -15,6 +24,9 @@ type BannerColors = {
 })
 export class ProfileSummaryComponent implements OnInit {
   form: FormGroup;
+
+  validFileExtensions: validFileExtension[] = ['png', 'jpg', 'jpeg'];
+  validMimeTypes: validMimeType[] = ['image/png', 'image/jpg', 'image/jpeg'];
 
   bannerColors: BannerColors = {
     colorOne: '#a0b4b7',
@@ -31,6 +43,49 @@ export class ProfileSummaryComponent implements OnInit {
     this.authService.userRole.pipe(take(1)).subscribe((role: Role) => {
       this.bannerColors = this.getBannerColors(role);
     });
+  }
+
+  onFileSelect(event: Event): void {
+    const file: File = (event.target as HTMLInputElement).files[0];
+    if (!file) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    from(file.arrayBuffer())
+      .pipe(
+        // eslint-disable-next-line @typescript-eslint/no-shadow
+        switchMap((buffer: Buffer) =>
+          from(fromBuffer(buffer)).pipe(
+            switchMap((fileTypeResult: FileTypeResult) => {
+              if (!fileTypeResult) {
+                //TODO: error handling
+                console.log({ error: ' file format not supported' });
+                return of();
+              }
+              const { ext, mime } = fileTypeResult;
+              const isFileTypeLegit = this.validFileExtensions.includes(
+                ext as any
+              );
+              const isMimeTypeLegit = this.validMimeTypes.includes(mime as any);
+              const isFileLegit = isFileTypeLegit && isMimeTypeLegit;
+              if (!isFileLegit) {
+                //TODO: error handling
+                console.log({
+                  error: 'file format does not match file extension',
+                });
+                return of();
+              }
+              return this.authService.uploadUserImage(formData);
+            })
+          )
+        )
+      )
+      .subscribe();
+
+    this.form.reset();
   }
 
   private getBannerColors(role: Role): BannerColors {
