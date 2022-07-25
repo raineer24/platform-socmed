5f096cd30c694e91f5c4f4812f875b16e434c09d
wtf <mura man kag wtf>
2022-07-26 00:13:19 +0800

    hasrequestbeensentorReceived

--- api/src/auth/services/user.service.ts
+++ api/src/auth/services/user.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { from, map, Observable } from 'rxjs';
+import { from, map, Observable, of, switchMap } from 'rxjs';
 import { Repository, UpdateResult } from 'typeorm';
 import { FriendRequestEntity } from '../models/friend-request.entity';
+import { FriendRequest } from '../models/friend-request.interface';
 import { User } from '../models/user.class';
 import { UserEntity } from '../models/user.entity';
 
@@ -42,6 +43,41 @@ export class UserService {
       map((user: User) => {
         delete user.password;
         return user.imagePath;
+      }),
+    );
+  }
+  hasRequestBeenSentOrReceived(
+    creator: User,
+    receiver: User,
+  ): Observable<boolean> {
+    return from(
+      this.friendRequestRepository.findOne({
+        where: [
+          { creator, receiver },
+          { creator: receiver, receiver: creator },
+        ],
+      }),
+    ).pipe(
+      switchMap((friendRequest: FriendRequest) => {
+        if (!friendRequest) {
+          return of(false);
+        }
+        return of(true);
+      }),
+    );
+  }
+
+  sendFriendRequest(
+    receiverId: number,
+    creator: User,
+  ): Observable<FriendRequest | { error: string }> {
+    if (receiverId === creator.id) {
+      return of({ error: 'It is not possible to add yourself' });
+    }
+
+    return this.findUserById(receiverId).pipe(
+      switchMap((receiver: User) => {
+        return;
       }),
     );
   }
