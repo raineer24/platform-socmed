c810cc8c15bd3f689fdbfc013f9a45423888b58b
wtf <mura man kag wtf>
2022-08-18 21:32:14 +0800

    validation

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+
+import * as fs from 'fs';
+import * as morgan from 'morgan';
+
+const logStream = fs.createWriteStream('api.log', {
+  flags: 'a',
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(morgan('tiny', { stream: logStream }));
   await app.listen(3000);
 }
 bootstrap();
