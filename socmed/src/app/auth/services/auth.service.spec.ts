0bf132fb9e5a6072e4e74fff7c443e987f879c3c
wtf <mura man kag wtf>
2022-09-08 17:25:33 +0800

    auth service spec ts

--- socmed/src/app/auth/services/auth.service.spec.ts
+++ socmed/src/app/auth/services/auth.service.spec.ts
@@ -3,52 +3,6 @@ import { NewUser } from '../models/newUser.model';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 import { User } from '../models/user.model';
-
-let httpClientSpy: { post: jasmine.Spy };
-let routerSpy: Partial<Router>;
-
-let authService: AuthService;
-
-const mockNewUser: NewUser = {
-  firstName: 'Eugene',
-  lastName: 'Sances',
-  email: 'eugenesanchez5@gmail.com  ',
-  password: 'password',
-};
-
-beforeEach(() => {
-  httpClientSpy = jasmine.createSpyObj('HtttpClient', ['post']);
-  routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
-
-  authService = new AuthService(httpClientSpy as any, routerSpy as any);
-});
-
-describe('AuthService', () => {
-  describe('register', () => {
-    it('should return the user', (done: DoneFn) => {
-      const expectedUser: User = {
-        id: 1,
-        firstName: 'Eugene',
-        lastName: 'Sances',
-        email: 'eugenesanchez5@gmail.com  ',
-        role: 'user',
-        imagePath: null,
-        posts: null,
-      };
-
-      httpClientSpy.post.and.returnValue(of(expectedUser));
-
-      authService
-        .register(mockNewUser)
-        .suauth.service.spec.tsbscribe((user: User) => {});
-    });
-  });
-});
-import { NewUser } from '../models/newUser.model';
-
-import { Router } from '@angular/router';
-import { AuthService } from './auth.service';
-import { User } from '../models/user.model';
 import { of } from 'rxjs';
 
 let httpClientSpy: { post: jasmine.Spy };
@@ -85,7 +39,16 @@ describe('AuthService', () => {
 
       httpClientSpy.post.and.returnValue(of(expectedUser));
 
-      authService.register(mockNewUser).subscribe((user: User) => {});
+      authService.register(mockNewUser).subscribe((user: User) => {
+        expect(typeof user.id).toEqual('number');
+        expect(user.firstName).toEqual(mockNewUser.firstName);
+        expect(user.lastName).toEqual(mockNewUser.lastName);
+        expect(user.email).toEqual(mockNewUser.email);
+        expect((user as any).password).toBeUndefined();
+        expect(user.role).toEqual('user');
+        expect(user.firstName).toBeNull();
+        expect(user.posts).toBeNull();
+      });
     });
   });
 });
