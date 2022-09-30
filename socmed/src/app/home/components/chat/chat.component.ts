8bfcb85ac380c8a8cfc45b40149bc9e7b83c0c79
raineer <delaritaraineer81@gmail.com>
2022-09-30 11:59:48 +0800

    friend my api

--- socmed/src/app/home/components/chat/chat.component.ts
+++ socmed/src/app/home/components/chat/chat.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { ChatService } from '../../services/chat.service';
+import { User } from 'src/app/auth/models/user.model';
 
 @Component({
   selector: 'app-chat',
@@ -12,12 +13,19 @@ export class ChatComponent implements OnInit {
   @ViewChild('form') form: NgForm;
   newMessage$: Observable<string>;
   messages: string[] = [];
+
+  friends: User[] = [];
   constructor(private chatService: ChatService) {}
 
   ngOnInit() {
-    return this.chatService.getNewMessage().subscribe((message: string) => {
+    this.chatService.getNewMessage().subscribe((message: string) => {
       this.messages.push(message);
     });
+
+    this.chatService.getFriends().subscribe((friends: User[]) => {
+      console.log(2, friends);
+      this.friends = friends;
+    })
   }
 
   onSubmit() {
