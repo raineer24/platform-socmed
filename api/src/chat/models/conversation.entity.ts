3a7e92bc0c41f2f634a43302a3427fd3a1ab1b6e
raineer <delaritaraineer81@gmail.com>
2022-10-02 18:00:59 +0800

    onetomany messageEntity

--- api/src/chat/models/conversation.entity.ts
+++ api/src/chat/models/conversation.entity.ts
@@ -3,9 +3,11 @@ import {
   Entity,
   JoinTable,
   ManyToMany,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { MessageEntity } from './message.entity';
 
 @Entity('conversation')
 export class ConversationEntity {
@@ -15,6 +17,9 @@ export class ConversationEntity {
   @ManyToMany(() => UserEntity)
   @JoinTable()
   users: UserEntity[];
+
+  @OneToMany(() => MessageEntity, (messageEntity) => messageEntity.conversation)
+  messages: MessageEntity[];
 
   @UpdateDateColumn()
   lastUpdated: Date;
