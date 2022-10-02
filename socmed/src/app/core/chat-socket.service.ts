c4c398bf2eea52876544f27c1aaf2e3915bf2cb4
raineer <delaritaraineer81@gmail.com>
2022-10-02 15:55:44 +0800

    socket gateway

--- socmed/src/app/core/chat-socket.service.ts
+++ socmed/src/app/core/chat-socket.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
 import { SocketIoConfig, Socket } from 'ngx-socket-io';
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: 'http://localhost:3000', 
+options: {
+  transportOptions: {
+    polling: {
+      extraHeaders: {
+        Authorization: localStorage.getItem('CapacitorStorage.token'),
+      },
+    },
+  }
+  
+} };
 
 @Injectable({
   providedIn: 'root'
