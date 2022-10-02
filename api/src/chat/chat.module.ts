d4d5432a49401690664f1ddc6639c2e41cad5dac
raineer <delaritaraineer81@gmail.com>
2022-10-02 16:54:56 +0800

    add module chat services api

--- api/src/chat/chat.module.ts
+++ api/src/chat/chat.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './gateaway/chat.gateway';
+import { ConversationService } from './services/conversation.service';
 
 @Module({
-  providers: [ChatGateway]
+  providers: [ChatGateway, ConversationService]
 })
 export class ChatModule {}
