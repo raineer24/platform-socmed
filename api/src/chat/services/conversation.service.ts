73fd8abe49a1187a96718ab44af0bada34eff25a
raineer <delaritaraineer81@gmail.com>
2022-10-04 17:27:09 +0800

    conversation method

--- api/src/chat/services/conversation.service.ts
+++ api/src/chat/services/conversation.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Observable } from 'rxjs';
 import { Repository } from 'typeorm';
 import { ActiveConversationEntity } from '../models/active-conversation.entity';
 import { ConversationEntity } from '../models/conversation.entity';
@@ -7,13 +8,17 @@ import { MessageEntity } from '../models/message.entity';
 
 @Injectable()
 export class ConversationService {
-    constructor() {
-        @InjectRepository(ConversationEntity)
-        private readonly conversationRepository: Repository<ConversationEntity>,
-        @InjectRepository(ActiveConversationEntity)
-        private readonly activeConversationRepository: Repository<ActiveConversationEntity>,
-        @InjectRepository(MessageEntity),
-        private readonly messageRE
-        
-    }
+  constructor(
+    @InjectRepository(ConversationEntity)
+    private readonly conversationRepository: Repository<ConversationEntity>,
+    @InjectRepository(ActiveConversationEntity)
+    private readonly activeConversationRepository: Repository<ActiveConversationEntity>,
+    @InjectRepository(MessageEntity)
+    private readonly messageRepository: Repository<MessageEntity>,
+  ) {}
+
+  getConversation(
+    creatorId: number,
+    friendId: number,
+  ): Observable< Conversation | undefined > {}
 }
