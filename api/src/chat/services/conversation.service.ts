fd877704ed7048f472c4ca9d96d29db63483e939
raineer <delaritaraineer81@gmail.com>
2022-10-04 23:36:17 +0800

    createConversation method()

--- api/src/chat/services/conversation.service.ts
+++ api/src/chat/services/conversation.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { from, Observable } from 'rxjs';
+import { from, map, Observable, of, switchMap } from 'rxjs';
+import { User } from 'src/auth/models/user.class';
 import { Repository } from 'typeorm';
 import { ActiveConversationEntity } from '../models/active-conversation.entity';
 import { ConversationEntity } from '../models/conversation.entity';
@@ -30,6 +31,31 @@ export class ConversationService {
         .orWhere('user.id = :friendId', { friendId })
         .having('COUNt(*) > 1')
         .getOne(),
+    ).pipe(map((conversation: Conversation) => conversation || undefined));
+  }
+
+  createConversation(creator: User, friend: User): Observable<Conversation> {
+    return this.getConversation(creator.id, friend.id).pipe(
+      switchMap((conversation: Conversation) => {
+        const doesConversationExist = !!conversation;
+        if (!doesConversationExist) {
+          const newConversation: Conversation = {
+            users: [creator, friend],
+          };
+          return from(this.conversationRepository.save(newConversation));
+        }
+        return of(conversation);
+      }),
     );
   }
+
+  getConversationsForUser() {}
+
+  getUsersInConversation() {}
+
+  getConversationsWithUsers() {}
+
+  joinConversation() {}
+
+
 }
