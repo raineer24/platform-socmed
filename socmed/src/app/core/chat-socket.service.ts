1044778d71b011be23971fa1a5668f341c2af1ee
raineer <delaritaraineer81@gmail.com>
2022-10-09 15:19:42 +0800

    core chat-socket.service

--- socmed/src/app/core/chat-socket.service.ts
+++ socmed/src/app/core/chat-socket.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
+
 import { SocketIoConfig, Socket } from 'ngx-socket-io';
-const config: SocketIoConfig = { url: 'http://localhost:3000', 
-options: {
-  transportOptions: {
-    polling: {
-      extraHeaders: {
-        Authorization: localStorage.getItem('CapacitorStorage.token'),
+
+const config: SocketIoConfig = {
+  url: 'http://localhost:3000',
+  options: {
+    transportOptions: {
+      polling: {
+        extraHeaders: {
+          Authorization: localStorage.getItem('CapacitorStorage.token'),
+        },
       },
     },
-  }
-  
-} };
+  },
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ChatSocketService extends Socket{
-
+export class ChatSocketService extends Socket {
   constructor() {
-    console.log("socket");
     super(config);
   }
 }
