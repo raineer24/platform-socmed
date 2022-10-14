3771ded3e7c6c5f53eb735b86ecda2d963802b3b
raineer <delaritaraineer81@gmail.com>
2022-10-14 23:23:40 +0800

    app.module ts

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prefer-const */
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -9,18 +8,10 @@ import { AuthModule } from './auth/auth.module';
 import { APP_FILTER } from '@nestjs/core';
 import { AllExceptionFilter } from './core/all-exceptions.filter';
 import { ChatModule } from './chat/chat.module';
-import { configuration } from 'config';
-let envFilePath = `${process.cwd()}/config/env/.env.development`;
-
-console.log(`Running in ${process.env.NODE_ENV}`);
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath,
-      load: [configuration],
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
